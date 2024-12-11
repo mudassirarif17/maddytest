ef31fb77bd61b9197a2c38e92d48159591836827
mudassir-arif <mudassir@aptechgdn.net>
2024-12-11 11:50:58 +0500

    initail commit

--- insert.php
+++ insert.php
@@ -1,29 +1,28 @@
 <?php 
+
+// Maddy code
 include('./connection.php');
 
 
 if(isset($_POST['sbt'])){
     $name = $_POST['name'];
     $p_id = $_POST['p_id'];
-    echo $name."<br>";  
-    echo $p_id."<br>";  
-    if (empty($p_id)) {
-        $p_id = NULL;
+
+    if($p_id == "NULL"){
+        $q = "insert into categories (name) values ('$name')";
+    }else{
+        $q = "insert into categories (name , p_id) values ('$name' , '$p_id')";
     }
-    echo $p_id."<br>";  
+    
+    
 
+    $result = mysqli_query($con , $q);
 
-    $q = "insert into categories (name) values ('$name')";
-
-$result = mysqli_query($con , $q);
-
-if(!$result){
-    die("Query Failed");
-}else{
-    echo "Query Success";
-    // header('location:./index.php?msg=Field Insert');
+    if(!$result){
+        die("Query Failed");
+    }else{
+        echo "Query Success";
+        header('location:./index.php?msg=Field Insert');
+    }
 }
-}
-
-
 ?>
